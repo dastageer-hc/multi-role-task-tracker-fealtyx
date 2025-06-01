00c5e03f6425ba8ad3f367f4a74aa2b2a5d040f5
Dastageer <dastageerh@DastageersAir2.lan>
2025-06-01 21:55:36 +0530

    funcationalities done,

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,10 @@ export default function RootLayout({
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='../assets/favicon.ico' />
       </head>
-      <body className={KumbhSans.className} suppressHydrationWarning>
+      <body
+        className={KumbhSans.className + " bg-gray-50"}
+        suppressHydrationWarning
+      >
         <AuthProvider>
           {children}
           <Toaster />
